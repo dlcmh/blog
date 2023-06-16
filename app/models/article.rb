4d69965d0f5ebe09bd142e3eda16d25e39943378
David Chin <dlcmhd@me.com>
2023-06-16 13:27:13 +0800

    status field in Article & Comment forms & controllers

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,14 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  # <start status>
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
+  # <end status>
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,13 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  # <start status>
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
+  # <end status>
 end
